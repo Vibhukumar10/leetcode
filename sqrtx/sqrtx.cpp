41d94f1fe4f0a473985ce4fad73858c5c6b0cf21
Vibhu <74215014+Vibhukumar10@users.noreply.github.com>
2021-09-12 23:35:32 +0530

    Time: 11 ms (20.77%), Space: 5.9 MB (24.66%) - LeetHub

--- sqrtx/sqrtx.cpp
+++ sqrtx/sqrtx.cpp
@@ -1,5 +1,20 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);    }
+        if(x==0)
+            return x;
+        int start=1, end=x,ans=0;
+        while(start<=end)
+        {
+            int mid=start + (end-start)/2;
+            if(mid<=x/mid)
+            {
+                ans=mid;
+                start=mid+1;
+            }
+            else
+                end=mid-1;
+        }
+        return ans;
+    }
 };
