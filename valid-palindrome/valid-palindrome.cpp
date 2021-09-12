7e48ce196e02cf14696ec4db7b25c30278cdb418
Vibhu <74215014+Vibhukumar10@users.noreply.github.com>
2021-09-12 23:52:42 +0530

    Time: 4 ms (93.47%), Space: 7.7 MB (18.78%) - LeetHub

--- valid-palindrome/valid-palindrome.cpp
+++ valid-palindrome/valid-palindrome.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
-    bool isPalindrome(string s) {
-        string s2;
-        for(int i=0;i<s.length();i++)
-        {
-            if(isalpha(s[i]))
-                s2.push_back(tolower(s[i]));
-            else if(isdigit(s[i]))
-                s2.push_back(s[i]);
-        }
-        stack<int> st;
-        for(char c:s2)
-            st.push(c);
-        string s3;
-        while(!st.empty())
-        {
-            s3.push_back(st.top());
-            st.pop();
-        }
-        return s2==s3;
+    bool isPalindrome(string str) {
+        
+       string s;
+        
+        for(char c : str)
+            if(isalnum(c)) 
+                s += isalpha(c) and isupper(c) ? tolower(c) : c;
+        
+        return equal(begin(s), end(s), rbegin(s));
     }
 };
