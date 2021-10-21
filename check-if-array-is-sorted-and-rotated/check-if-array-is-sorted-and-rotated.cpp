58b29b09e28e95084b1d6df66dc45e3c24b74512
Vibhu <74215014+Vibhukumar10@users.noreply.github.com>
2021-10-21 14:47:20 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (78.00%) - LeetHub

--- check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -4,10 +4,8 @@ public:
         int count=0;
         int n=nums.size();
         for(int i=0;i<n;i++)
-        {
             if(nums[i]>nums[(i+1)%n])
                 count++;
-        }
         
         return count<=1;
     }
