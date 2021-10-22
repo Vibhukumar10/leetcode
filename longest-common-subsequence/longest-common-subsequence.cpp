cbdb073671ca832407913fe81c83a99045ca9010
Vibhu <74215014+Vibhukumar10@users.noreply.github.com>
2021-10-22 21:07:31 +0530

    Time: 12 ms (82.41%), Space: 10.6 MB (72.41%) - LeetHub

--- longest-common-subsequence/longest-common-subsequence.cpp
+++ longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:
+    
+//     int fun(string s1,string s2,int m,int n)
+//     {
+//         if(n==0||m==0)
+//             return 0;
+        
+//         if(s1[m-1]==s2[n-2])
+//             return 1+fun(s1,s2,m-1,n-1);
+//         else
+//             return max(fun(s1,s2,m-1,n),fun(s1,s2,m,n-1));
+//     }
+    
     int longestCommonSubsequence(string text1, string text2) {
-        int m=text1.length(),n=text2.length();
-        int dp[m+1][n+1];//dp initially empty
-        //first row and column will be empty
-        for(int i=0;i<m+1;i++)
-            dp[i][0]=0;
-        for(int j=0;j<n+1;j++)
-            dp[0][j]=0;
-        //fill the remaining dp according to the recursive logic
-        for(int i=1;i<m+1;i++)
+        int m=text1.size();
+        int n=text2.size();
+        
+        int dp[m+1][n+1];
+        
+        for(int i=0;i<=m;i++)
         {
-            for(int j=1;j<n+1;j++)
+            for(int j=0;j<=n;j++)
             {
-                if(text1[i-1]==text2[j-1])
-                    dp[i][j]=1+dp[i-1][j-1];
+                if(i==0 || j==0)
+                    dp[i][j]=0;
+                
                 else
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                {
+                    if(text1[i-1]==text2[j-1])
+                        dp[i][j]=dp[i-1][j-1]+1;
+                    else
+                        dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
             }
         }
         return dp[m][n];
