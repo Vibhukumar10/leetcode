131e7b45e42978ef73a5171cb06858c94b37498e
Vibhu <74215014+Vibhukumar10@users.noreply.github.com>
2021-11-02 03:25:12 +0530

    Time: 8 ms (91.17%), Space: 10.3 MB (85.29%) - LeetHub

--- longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,27 +1,61 @@
 class Solution {
 public:
-    int helper(vector<int> &arr,int n)
+//     int helper(vector<int> &arr,int n)
+//     {
+//         int res=1;
+//         vector<int> lis(n,1);
+        
+//         for(int i=1;i<n;i++)
+//         {
+//             for(int j=0;j<i;j++)
+//             {
+//                 if(arr[j]<arr[i])
+//                     lis[i]=max(lis[i],lis[j]+1);
+//             }
+//         }
+        
+//         for(int i=0;i<n;i++)
+//             res=max(res,lis[i]);
+        
+//         return res;
+//     }
+    
+    int getCeil(int arr[],int l,int r,int x)
     {
-        int res=1;
-        vector<int> lis(n,1);
-        
-        for(int i=1;i<n;i++)
+        while(l<r)
         {
-            for(int j=0;j<i;j++)
-            {
-                if(arr[j]<arr[i])
-                    lis[i]=max(lis[i],lis[j]+1);
-            }
+            int m=l+(r-l)/2;
+            
+            if(arr[m]<x)
+                l=m+1;
+            
+            else
+                r=m;           
         }
+        return r;
         
-        for(int i=0;i<n;i++)
-            res=max(res,lis[i]);
-        
-        return res;
     }
     
-    
     int lengthOfLIS(vector<int>& nums) {
-        return helper(nums,nums.size());
+        // return helper(nums,nums.size());
+        
+        int n=nums.size();
+        int tail[n],len=1;
+        
+        tail[0]=nums[0];
+        for(int i=1;i<n;i++)
+        {
+            if(nums[i]>tail[len-1])
+            {
+                tail[len]=nums[i];
+                len++;
+            }
+            else
+            {
+                int ceilIdx=getCeil(tail,0,len-1,nums[i]);
+                tail[ceilIdx]=nums[i];
+            }
+        }
+        return len;
     }
 };
