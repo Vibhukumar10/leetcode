b95598bd11df4b5a3e0c758b164152f4bab78559
Vibhu <74215014+Vibhukumar10@users.noreply.github.com>
2021-11-09 15:21:12 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (91.29%) - LeetHub

--- swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
+++ swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
@@ -12,13 +12,34 @@ class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
         
-        if(!head || !head->next)
-            return head;
+//         if(!head || !head->next)
+//             return head;
                 
-        ListNode *newHead=head->next;
-        head->next=swapPairs(newHead->next);
-        newHead->next=head;
+//         ListNode *newHead=head->next;
+//         head->next=swapPairs(newHead->next);
+//         newHead->next=head;
         
-        return newHead;
+//         return newHead;
+        
+        if(head==NULL || head->next==NULL)
+        return head;
+        //make next store the node which is 2 nodes ahead
+        ListNode *curr=head->next->next;
+        //handling the first two nodes
+        ListNode *prev=head;
+        head=head->next;
+        head->next=prev;
+        //handling the remaining nodes 
+        while(curr!=NULL && curr->next!=NULL)
+        {
+            prev->next=curr->next;
+            prev=curr;
+            ListNode *next=curr->next->next;
+            curr->next->next=curr;
+            //set curr as next (move ahead by 2 nodes)
+            curr=next;
+        }
+        prev->next=curr;
+        return head;
     }
 };
