4f1e50da3a3f169a77b5212b66dc472b06c3435d
Vibhu <74215014+Vibhukumar10@users.noreply.github.com>
2021-11-18 17:35:39 +0530

    Time: 0 ms (100.00%), Space: 6 MB (21.58%) - LeetHub

--- first-bad-version/first-bad-version.cpp
+++ first-bad-version/first-bad-version.cpp
@@ -5,15 +5,15 @@ class Solution {
 public:
     int firstBadVersion(int n) {
         int l=1,r=n;
-        while(l<=r)
+        while(l<r)
         {
             int m=l+(r-l)/2;
             if(isBadVersion(m))
-                r=m-1;
-            
+                r=m;
             else
                 l=m+1;
         }
-        return l;
+        if(isBadVersion(l)) return l;
+        return -1;
     }
 };
