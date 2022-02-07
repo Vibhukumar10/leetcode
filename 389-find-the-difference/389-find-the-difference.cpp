63176e4c8f269b20835235c38d6d44d1581ef670
Vibhu <74215014+Vibhukumar10@users.noreply.github.com>
2022-02-07 22:06:14 +0530

    Time: 4 ms (67.41%), Space: 6.7 MB (25.35%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        vector<int> v(26,0),w(26,0);
+        vector<int> v(26,0);
         for(int i=0;i<s.length();i++)
         {
             v[s[i]-'a']++;
+            v[t[i]-'a']--;
         }
+        v[t[t.length()-1]-'a']--;
         
-        for(int i=0;i<t.length();i++)
-        {
-            w[t[i]-'a']++;
-            if(w[t[i]-'a']>v[t[i]-'a'])
-                return t[i];
-        }
+        for(int i=0;i<26;i++)
+            if(v[i]==-1)
+                return (i+'a');
+        
         return '0';
     }
 };
