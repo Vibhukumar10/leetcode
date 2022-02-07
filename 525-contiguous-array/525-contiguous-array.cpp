c845e93e9c47b7d36802504d8ee59e92ca5fb50d
Vibhu <74215014+Vibhukumar10@users.noreply.github.com>
2022-02-07 20:54:47 +0530

    Time: 112 ms (91.69%), Space: 83.9 MB (30.08%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.cpp
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -5,12 +5,14 @@ public:
         if(nums.size()==1)
             return 0;
         unordered_map<int,int> m;
-        m.insert({0,-1});
+        // m.insert({0,-1});
         
         int curr=0;
         for(int i=0;i<n;i++)
         {
             curr+=nums[i]==1?1:-1;
+            if(curr==0)
+                res=max(res,i+1);
             if(m.count(curr))
                 res=max(res,i-m[curr]);
             else
