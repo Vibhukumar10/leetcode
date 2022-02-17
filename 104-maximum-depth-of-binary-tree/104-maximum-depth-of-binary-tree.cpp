b79e2826cb5e13b7b323d92ab0f960686de147e2
Vibhu <74215014+Vibhukumar10@users.noreply.github.com>
2022-02-17 18:54:54 +0530

    Time: 8 ms (78.73%), Space: 19 MB (14.51%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
@@ -15,6 +15,24 @@ public:
         if(!root)
             return 0;
         
-        return 1+max(maxDepth(root->left),maxDepth(root->right));
+        int res=0;
+
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int sz=q.size();
+            for(int i=0;i<sz;i++)
+            {
+                TreeNode *temp=q.front();
+                q.pop();
+                
+                if(temp->left) q.push(temp->left);
+                if(temp->right) q.push(temp->right);
+            }
+            res++;
+        }
+        
+        return res;       
     }
 };
