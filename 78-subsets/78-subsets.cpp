1a8ffdd9faa594d21b59cef4fdb942b6c40934aa
Vibhu <74215014+Vibhukumar10@users.noreply.github.com>
2022-02-17 19:06:37 +0530

    Time: 3 ms (68.75%), Space: 7.3 MB (41.81%) - LeetHub

---
+++ 78-subsets/78-subsets.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> getArray(int num,vector<int> nums)
+    {
+        vector<int> res;
+        int index=nums.size()-1;
+        while(num)
+        {
+            if(num&1 == 1)
+            {
+                res.push_back(nums[index]);
+            }
+            index--;
+            num=num>>1;
+        }
+        return res;
+    }
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res;
+        int n=nums.size();
+        
+        for(int i=0;i<pow(2,n);i++)
+        {
+            vector<int> set=getArray(i,nums);
+            res.push_back(set);
+        }
+        return res;
+    }
+};
