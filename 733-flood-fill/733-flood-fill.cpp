7a027f9670aec1760a3094a8b953667e38a4e567
Vibhu <74215014+Vibhukumar10@users.noreply.github.com>
2022-04-25 19:46:03 +0530

    Time: 11 ms (68.26%), Space: 13.8 MB (91.99%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -3,10 +3,10 @@ public:
     void flood(vector<vector<int>> &image,int r,int c,int color,int newColor) {
         if(image[r][c] == color) {
             image[r][c]=newColor;
-            if (r >= 1) flood(image, r-1, c, color, newColor);
-            if (c >= 1) flood(image, r, c-1, color, newColor);
-            if (r+1 < image.size()) flood(image, r+1, c, color, newColor);
-            if (c+1 < image[0].size()) flood(image, r, c+1, color, newColor);
+            if(r >= 1) flood(image, r-1, c, color, newColor);
+            if(c >= 1) flood(image, r, c-1, color, newColor);
+            if(r+1 < image.size()) flood(image, r+1, c, color, newColor);
+            if(c+1 < image[0].size()) flood(image, r, c+1, color, newColor);
         }
     }
     
