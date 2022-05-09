6042f5a9aaa08e95f502867912810aae0efeca4b
Vibhu <74215014+Vibhukumar10@users.noreply.github.com>
2022-05-09 20:02:50 +0530

    Added solution - LeetHub

--- Bridge edge in a graph - GFG/bridge-edge-in-a-graph.cpp
+++ Bridge edge in a graph - GFG/bridge-edge-in-a-graph.cpp
@@ -36,58 +36,40 @@ class Solution
         // return res;
     }
     
-    // void dfs(int s,vector<bool> &vis,int c,int d,vector<int> adj[]) {
-    //     vis[s]=true;
+    void dfs(int s,vector<bool> &vis,int c,int d,vector<int> adj[]) {
+        vis[s]=true;
         
-    //     for(int v:adj[s]) {
-    //         if((s==c && v==d) || (s==d && v==c)) {
-    //             continue;
-    //         } else {
-    //             if(!vis[v]) {
-    //                 dfs(v,vis,c,d,adj);
-    //             }
-    //         }
-    //     }
-    // }
+        for(int v:adj[s]) {
+            if(!vis[v]) {
+                dfs(v,vis,c,d,adj);
+            }
+        }
+    }
     
     int isBridge(int V, vector<int> adj[], int c, int d) 
     {
         // Code here
         vector<bool> vis(V,false);
         // bfs(0,vis,c,d,adj);
-        queue<int> q;
-        q.push(c);
-        vis[c]=true;
-        // int res=0;
-        
-        while(!q.empty()) {
-            int sz=q.size();
-            for(int i=0;i<sz;i++) {
-                int u=q.front();
-                q.pop();
-                
-                // res++;
-                
-                for(int v:adj[u]) {
-                    if((u==d && v==c) || (u==c && v==d)) {
-                        continue;
-                    } else {
-                        if(!vis[v]) {
-                            q.push(v);
-                            vis[v]=true;
-                        }
-                    }
-                }
+        int init=0,res=0;
+        for(int i=0;i<V;i++) {
+            if(!vis[i]) {
+                dfs(i,vis,c,d,adj);
+                init++;
             }
         }
         
-        // for(int i=0;i<V;i++) {
-        //     if(vis[i]==false) {
-        //         return 1;
-        //     }
-        // }
+        for(int i=0;i<V;i++) {
+            vis[i]=false;
+        }
         
-        return vis[d] ? 0:1;
+        for(int i=0;i<V;i++) {
+            if(!vis[i]) {
+                bfs(i,vis,c,d,adj);
+                res++;
+            }
+        }
+        return !(res==init);
     }
 };
 
