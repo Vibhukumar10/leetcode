641ee00bd9f4790b1addfb7da21ae9a874688619
Vibhu <74215014+Vibhukumar10@users.noreply.github.com>
2022-05-27 16:54:33 +0530

    Time: 40 ms (8.69%), Space: 17.3 MB (6.60%) - LeetHub

---
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool dfs(int s,int fs,int d,vector<int> adj[],vector<bool> vis) {
+        vis[s]=true;
+        
+        if(s==d) return true;
+        
+        for(int v:adj[s]) {
+            if(s==fs && v==d) continue;
+            if(!vis[v]) {
+                if(dfs(v,fs,d,adj,vis)) return true;
+            }
+        }
+        return false;
+    }
+    
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        vector<int> adj[edges.size()+1];
+        
+        for(auto edge:edges) {
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+        }
+        
+        vector<bool> vis(edges.size()+1,false);
+        
+        for(int i=edges.size()-1;i>=0;i--) {
+            if(dfs(edges[i][0],edges[i][0],edges[i][1],adj,vis)) {
+                return {edges[i][0],edges[i][1]};
+            }
+        }
+        return {};
+    }
+};
