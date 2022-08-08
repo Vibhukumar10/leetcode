6f0fbc07b520b194b8dd203a93cbbf459c2ae420
Vibhu <74215014+Vibhukumar10@users.noreply.github.com>
2022-08-08 23:11:05 +0530

    Time: 17 ms (18.00%), Space: 13.6 MB (78.16%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
-    void helper(int &res,int curr,vector<int> &cost,int ind) {
-        if(ind>=cost.size()) {
-            res=min(res,curr);
-            return;
-        }
+//     int helper(vector<int> &cost,int n) {
+//         if(n<=0) {
+//             return 0;
+//         }
         
-        curr+=cost[ind];
-        helper(res,curr,cost,ind+1);        
-        helper(res,curr,cost,ind+2);        
-    }
+//         int pick = cost[n-1] + helper(cost,n-1);
+//         int np = helper(cost,n-2);
+//         // int np = 1e9;
+        
+//         return min(pick,np);
+//     }
+    
     
     int minCostClimbingStairs(vector<int>& cost) {
         // int res=INT_MAX;
@@ -17,13 +19,19 @@ public:
         // helper(res,0,cost,0);
         // helper(res,0,cost,1);
         // return res;
+        
+        // return min(helper(cost,cost.size()-1),helper(cost,cost.size()-2));
+        
         int n=cost.size();
         vector<int> dp(n+1,0);
-        dp[0]=cost[0];
-        dp[1]=cost[1];
-        for(int i=2;i<n;i++) {
-            dp[i]=cost[i]+min(dp[i-1],dp[i-2]);
+        dp[0]=0;
+        dp[1]=cost[0];
+        // dp[2]=min(cost[0],cost[1]);
+        dp[2]=cost[1];
+        for(int i=3;i<=n;i++) {
+            dp[i]=cost[i-1]+min(dp[i-1],dp[i-2]);
         }
-        return min(dp[n-1],dp[n-2]);
+        return min(dp[n],dp[n-1]);
+        // return dp[n];
     }
 };
