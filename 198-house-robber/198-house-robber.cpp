d5c39d30894bd4bab1de6c957aab35c974b49126
Vibhu <74215014+Vibhukumar10@users.noreply.github.com>
2022-08-10 12:38:33 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (48.07%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -7,17 +7,17 @@ public:
             return nums[0];
         }
         
-        vector<int> dp(nums.size()+1,0);
+        vector<int> dp(3,0);
         
         dp[0] = 0;
         dp[1] = nums[0];
         dp[2] = max(nums[0],nums[1]);
         
         for(int i=3;i<=nums.size();i++) {
-            int pick = nums[i-1] + dp[i-2];
-            int np = dp[i-1];
-            dp[i] = max(pick,np);
+            int pick = nums[i-1] + dp[(i-2)%3];
+            int np = dp[(i-1)%3];
+            dp[i%3] = max(pick,np);
         }
-        return max(dp[n],dp[n-1]);
+        return max(dp[n%3],dp[(n-1)%3]);
     }
 };
