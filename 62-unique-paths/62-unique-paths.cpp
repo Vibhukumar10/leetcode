5f682a525df2ed12bfbbc3c35261fd15e7a34118
Vibhu <74215014+Vibhukumar10@users.noreply.github.com>
2022-08-19 02:05:36 +0530

    Time: 5 ms (18.49%), Space: 6.6 MB (26.81%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -17,8 +17,19 @@ public:
     }
     
     int uniquePaths(int m, int n) {
-        vector<vector<int>> vis(m,vector<int> (n,0));
-        vector<vector<int>> dp(m,vector<int> (n,-1));
-        return dfs(0,0,m,n,vis,dp);
+        // vector<vector<int>> vis(m,vector<int> (n,0));
+        // vector<vector<int>> dp(m,vector<int> (n,-1));
+        vector<vector<int>> dp (m, vector<int> (n,1));
+        
+        for(int j=1;j<n;j++) {
+            for (int i=1;i<m;i++) {
+                dp[i][j] = dp[i][(j-1)] + dp[(i-1)][j];
+            }
+            
+        }
+        
+        return dp[(m-1)][n-1];
+        
+        // return dfs(0,0,m,n,vis,dp);
     }
 };
