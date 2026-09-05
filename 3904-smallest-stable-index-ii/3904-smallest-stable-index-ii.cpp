b6a170b9ed5f8984b0b8d245e4d289ffe6fc9501
Vibhu <74215014+Vibhukumar10@users.noreply.github.com>
2026-09-05 20:16:19 +0530

    Time: 13 ms (45.79%), Space: 211 MB (11.7%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -3,24 +3,22 @@ public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n=nums.size();
 
-        deque<int> dq;
-        for(int i=0;i<n;i++) {
-            while(!dq.empty() && nums[dq.back()] > nums[i]) {
-                dq.pop_back();
-            }
-            dq.push_back(i);
-        }
-
-        int currMax=nums[0],currMin=nums[dq.front()];
-        if(currMax-currMin<=k) return 0;
+        vector<int> lMax(n),rMin(n);
+        lMax[0]=nums[0];
+        rMin[n-1]=nums[n-1];
 
         for(int i=1;i<n;i++) {
-            currMax=max(currMax,nums[i]);
-            if(i-1==dq.front()) {
-                dq.pop_front();
+            lMax[i]=max(lMax[i-1],nums[i]);
+        }
+
+        for(int i=n-2;i>=0;i--) {
+            rMin[i]=min(rMin[i+1],nums[i]);
+        }
+
+        for(int i=0;i<n;i++) {
+            if(lMax[i]-rMin[i]<=k) {
+                return i;
             }
-            currMin=nums[dq.front()];
-            if(currMax-currMin<=k) return i;
         }
 
         return -1;
